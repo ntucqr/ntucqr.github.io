ea87f1bcb3d88186eff679f50403d92f75d1f07e
lwx <lwx@is.me>
2020-03-28 15:08:03 +0800

    new PWA.

---
+++ precache-manifest.c2c3e2cd7418acbc02c09cc5fcd9912d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fa9205568a526cd8df557c7117f722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d5f6113487e54eb2993",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "957d18d4ee95bf1fae4e",
+    "url": "/static/js/2.7168ee4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7168ee4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5f6113487e54eb2993",
+    "url": "/static/js/main.0da05746.chunk.js"
+  },
+  {
+    "revision": "014e962ba3dd6aecc9fc",
+    "url": "/static/js/runtime-main.623df2ad.js"
+  }
+]);
